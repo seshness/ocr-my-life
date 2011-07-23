22993140743072f600b022d52ef0d97eeff2032c
Ben Dong <bdong281@gmail.com>
2011-07-22 22:30:45 -0700

    mobile user

--- bill/views.py
+++ bill/views.py
@@ -21,6 +21,8 @@ mobile_ua_hints = [ 'SymbianOS', 'Opera Mini', 'iPhone' ]
 def mobileBrowser(request):
     ''' Super simple device detection, returns True for mobile devices '''
  
+    return True
+ '''
     mobile_browser = False
     ua = request.META['HTTP_USER_AGENT'].lower()[0:4]
  
@@ -32,7 +34,7 @@ def mobileBrowser(request):
                 mobile_browser = True
  
     return mobile_browser
- 
+ '''
  
 def index(request):
     '''Render the index page'''
