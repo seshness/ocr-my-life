1c7ef6b718ed8b7b4134e7d209594260962b1c67
Seshadri Mahalingam <seshadri@fb.com>
2011-07-23 02:14:50 -0700

    JS!!

---
+++ bill/static/js/billsplit.js
@@ -0,0 +1,25 @@
+var BillSplit = {};
+
+BillSplit.addPerson = function(name, colour) {
+  var people = localStorage.getItem('people');
+  if (!people) {
+    people = [];
+    var id = 1;
+  } else {
+    people = JSON.parse(people);
+    var id = people.length;
+  }
+  people.push({"id":id, "name":name, "colour":colour});
+  localStorage.setItem('people', JSON.stringify(people));
+
+  $('#people-list').append(
+      '<li data-theme="c" class="ui-btn ui-btn-icon-right ui-li ui-btn-up-c"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="index.html" class="ui-link-inherit">' +
+      name +
+      '<span id="' +
+      id + '-count"' +
+      ' class="ui-li-count ui-btn-up-c ui-btn-corner-all">$0.00</span></a></div><span class="ui-icon ui-icon-arrow-r"></span></div></li>'
+                           );
+
+  $('ul').listview('refresh');
+};
+
