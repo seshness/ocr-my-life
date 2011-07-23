1313f3f4da80fcf9acade7b0dd88710f66788b04
Ben Dong <bdong281@gmail.com>
2011-07-22 22:12:20 -0700

    first

---
+++ bill/views.py
@@ -0,0 +1,47 @@
+# Some standard Django stuff
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import Context, loader
+ 
+# list of mobile User Agents
+mobile_uas = [
+	'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
+	'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
+	'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
+	'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
+	'newt','noki','oper','palm','pana','pant','phil','play','port','prox',
+	'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
+	'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
+	'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
+	'wapr','webc','winw','winw','xda','xda-'
+	]
+ 
+mobile_ua_hints = [ 'SymbianOS', 'Opera Mini', 'iPhone' ]
+ 
+ 
+def mobileBrowser(request):
+    ''' Super simple device detection, returns True for mobile devices '''
+ 
+    mobile_browser = False
+    ua = request.META['HTTP_USER_AGENT'].lower()[0:4]
+ 
+    if (ua in mobile_uas):
+        mobile_browser = True
+    else:
+        for hint in mobile_ua_hints:
+            if request.META['HTTP_USER_AGENT'].find(hint) > 0:
+                mobile_browser = True
+ 
+    return mobile_browser
+ 
+ 
+def index(request):
+    '''Render the index page'''
+ 
+    if mobileBrowser(request):
+        t = loader.get_template('m_index.html')
+    else:
+        t = loader.get_template('index.html')
+ 
+    c = Context( { }) # normally your page data would go here
+ 
+    return HttpResponse(t.render(c))
